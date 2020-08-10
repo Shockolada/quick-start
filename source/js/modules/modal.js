e2d46ef96ba74f70d76341b778ea0dd0a7d6abdf
Shockolada <shockolada@gmail.com>
2020-08-10 17:32:04 +0300

    Menu module

--- source/js/modules/modal.js
+++ source/js/modules/modal.js
@@ -1,10 +1,13 @@
+import calcScrollWidth from '../services/calc-scroll-width';
+
 const modals = () => {
   function bindModal(triggerSelector, modalSelector, closeSelector) {
 
     const trigger = document.querySelectorAll(triggerSelector),
       modal = document.querySelector(modalSelector),
       close = document.querySelectorAll(closeSelector),
-      overlay = document.querySelector('.modal-overlay');
+      overlay = document.querySelector('.modal-overlay'),
+      scrollWidth = calcScrollWidth();
 
     trigger.forEach(item => {
       item.addEventListener('click', (evt) => {
@@ -12,7 +15,7 @@ const modals = () => {
           evt.preventDefault();
         }
 
-        openModal(modal, overlay);
+        openModal();
       });
     });
 
@@ -22,39 +25,45 @@ const modals = () => {
           evt.preventDefault();
         }
 
-        closeModal(modal, overlay);
+        closeModal();
       });
     });
 
-    modal.addEventListener('click', (evt) => {
-      if (evt.target === modal) {
-        closeModal(modal, overlay);
+    try {
+      modal.addEventListener('click', (evt) => {
+        if (evt.target === modal) {
+          closeModal();
+        }
+      });
+    } catch (err) {}
+
+    function openModal() {
+      document.body.style.paddingRight = `${scrollWidth}px`;
+      modal.style.paddingRight = `${scrollWidth}px`;
+      modal.classList.add('active');
+      overlay.classList.add('active');
+
+      document.body.classList.add('scroll-lock');
+      document.addEventListener('keydown', onEscPress);
+    }
+
+    function closeModal() {
+      document.body.style.paddingRight = '';
+      modal.style.paddingRight = '';
+      modal.classList.remove('active');
+      overlay.classList.remove('active');
+
+      document.body.classList.remove('scroll-lock');
+      document.removeEventListener('keydown', onEscPress);
+    }
+
+    function onEscPress(evt) {
+      if (evt.which === 27 && modal.classList.contains('active')) {
+        closeModal();
       }
-    });
-  }
-
-  function openModal(modal, overlay) {
-    modal.classList.add('active');
-    document.body.classList.add('scroll-lock');
-    overlay.classList.add('active');
-
-    document.addEventListener('keydown', closeByEsc);
-  }
-
-  function closeModal(modal, overlay) {
-    document.body.classList.remove('scroll-lock');
-    modal.classList.remove('active');
-    overlay.classList.remove('active');
-
-    document.removeEventListener('keydown', closeByEsc);
-  }
-
-  function closeByEsc(evt) {
-    console.log('closed');
-    if (evt.which === 27 && modal.classList.contains('active')) {
-      closeModal(modal, overlay);
     }
   }
+
 
   bindModal('[data-trigger-modal', '#modal', '.modal__close');
   bindModal('[data-trigger-modal-2', '#modal-2', '.modal__close');
